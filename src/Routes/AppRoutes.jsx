fae94a3de6353c7680b7e5d40d642ab031b997dd
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-12-22 11:45:19 +0530

    add to new component in route

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,17 +1,23 @@
-import { useContext, useEffect, useState } from "react";
 import { Layout } from "../Layout";
-import { getProducts } from "../data/product";
-
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import WishList from "../Component/WishList/WishList";
+import Home from "../Component/Home/Home";
+import Cart from "../Component/Cart/Cart";
 
 const AppRoutes = () => {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<Layout />}></Route>)
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/wishlist" element={<WishList />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Route>
+    )
   );
 
   return (
