849a7b0aabf9d00fcb27023222dc5b9631498641
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-12-22 11:47:12 +0530

    create new wishlist component

---
+++ src/Component/WishList/WishList.jsx
@@ -0,0 +1,62 @@
+import { useContext } from "react";
+import { Heart, ShoppingCart, Trash2 } from "lucide-react";
+import { ShopContext } from "../../Context/shopContext";
+
+const WishList = () => {
+  const { wishlist, addToCart, removeFromWishlist } = useContext(ShopContext);
+
+  // console.log(wishlist);
+
+  return (
+    <>
+      {!wishlist.length && (
+        <div className="text-center mt-2.5 font-bold"> No wishlist </div>
+      )}
+      {/* <>{Array.isArray(wishlist) ? wishlist[0].title : "no"} </> */}
+      {wishlist?.map((list, index) => {
+        return (
+          <div
+            key={list.id}
+            className="border rounded-xl p-4 shadow-sm hover:shadow-md transition"
+          >
+            <div className="relative">
+              <img
+                src={list.image}
+                alt={list.name}
+                className="w-full h-40 object-contain"
+              />
+
+              <button
+                onClick={() => removeFromWishlist(list.id)}
+                className="absolute top-2 right-2 text-red-500"
+              >
+                <Heart fill="red" />
+              </button>
+            </div>
+
+            <h3 className="font-semibold mt-3">{list.title}</h3>
+            <p className="text-green-600 font-bold">₹{list.price}</p>
+
+            <div className="flex gap-2 mt-4">
+              <button
+                onClick={() => addToCart(list)}
+                className="flex-1 bg-orange-500 text-white py-2 rounded-lg flex items-center justify-center gap-2 cursor-pointer"
+              >
+                <ShoppingCart size={18} /> Add to Cart
+              </button>
+
+              <button
+                onClick={() => removeFromWishlist(list.id)}
+                className="bg-red-100 text-red-600 p-2 rounded-lg cursor-pointer"
+              >
+                <Trash2 size={18} />
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default WishList;
